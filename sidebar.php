b91e91819992747bbe0d9cab174f0fa655db9a96
Konstantin Kovshenin <kovshenin@gmail.com>
2012-08-29 10:54:04 +0400

    removing default widgets, renaming action

--- sidebar.php
+++ sidebar.php
@@ -7,28 +7,6 @@
  */
 ?>
 		<div id="secondary" class="widget-area" role="complementary">
-			<?php do_action( 'before_sidebar' ); ?>
-			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
-
-				<aside id="search" class="widget widget_search">
-					<?php get_search_form(); ?>
-				</aside>
-
-				<aside id="archives" class="widget">
-					<h1 class="widget-title"><?php _e( 'Archives', 'publish' ); ?></h1>
-					<ul>
-						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-					</ul>
-				</aside>
-
-				<aside id="meta" class="widget">
-					<h1 class="widget-title"><?php _e( 'Meta', 'publish' ); ?></h1>
-					<ul>
-						<?php wp_register(); ?>
-						<li><?php wp_loginout(); ?></li>
-						<?php wp_meta(); ?>
-					</ul>
-				</aside>
-
-			<?php endif; // end sidebar widget area ?>
+			<?php do_action( 'publish_before_sidebar' ); ?>
+			<?php dynamic_sidebar( 'sidebar-1' ) ?>
 		</div><!-- #secondary .widget-area -->
