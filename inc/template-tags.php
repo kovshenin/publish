560055e233511dac24126566d1af2ed4c22f68d0
Konstantin Kovshenin <kovshenin@gmail.com>
2012-11-19 11:21:15 +0400

    Filter wp_title to generate a better <title>

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -160,3 +160,30 @@ function publish_category_transient_flusher() {
 }
 add_action( 'edit_category', 'publish_category_transient_flusher' );
 add_action( 'save_post', 'publish_category_transient_flusher' );
+
+/**
+ * Filters wp_title to print a neat <title> tag based on what is being viewed.
+ *
+ * @since Publish 1.2.1
+ */
+function publish_wp_title( $title, $sep ) {
+	global $page, $paged;
+
+	if ( is_feed() )
+		return $title;
+
+	// Add the blog name
+	$title .= get_bloginfo( 'name' );
+
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		$title .= " $sep $site_description";
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 )
+		$title .= " $sep " . sprintf( __( 'Page %s', 'publish' ), max( $paged, $page ) );
+
+	return $title;
+}
+add_filter( 'wp_title', 'publish_wp_title', 10, 2 );
