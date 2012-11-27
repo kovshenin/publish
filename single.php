fe8b1b3c731d323062472f4810804bd737c10718
Konstantin Kovshenin <kovshenin@gmail.com>
2012-11-27 19:03:22 +0400

    Remove nav-above from single.php and search.php.

--- single.php
+++ single.php
@@ -13,8 +13,6 @@ get_header(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php publish_content_nav( 'nav-above' ); ?>
-
 				<?php get_template_part( 'content', 'single' ); ?>
 
 				<?php publish_content_nav( 'nav-below' ); ?>
